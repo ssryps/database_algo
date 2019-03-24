f3f8ccd384025274ac1b4316bb84dc762b40904a
ssryps <825160150@qq.com>
2019-03-24 23:51:22 +0800

    some umimportant files

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mason/cs/code/database_algo
+# Build directory: /home/mason/cs/code/database_algo/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mason/CS/Code/database_algo
+# Install script for directory: /home/mason/cs/code/database_algo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mason/CS/Code/database_algo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mason/cs/code/database_algo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
